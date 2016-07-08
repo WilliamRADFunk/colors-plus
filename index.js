eed9048f21067ee9ff53b0f97bb52e2b8eee00b7
William Funk <wfunk@knights.ucf.edu>
2016-07-08 10:10:00 -0400

    Added the failure option.

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ var colors = require('colors');
 
 exports["success"] = function(msg)
 {
-	return ('\u2713'.green + msg);
+	return ('\u2713'.green + "\t" + msg);
+};
+exports["failure"] = function(msg)
+{
+	return ('\u2717'.red + "\t" + msg);
 };
